7bc7a20f73f698d8f40276299b735c6f4dc271b6
jiangxch <jiangxch8355@fxiaoke.com>
2020-02-29 14:45:48 +0800

    添加CROS跨域

---
+++ course-learning-management-biz/src/main/java/com/github/jiangxch/courselearningmanagement/biz/filter/WebMvcConfiguration.java
@@ -0,0 +1,36 @@
+package com.github.jiangxch.courselearningmanagement.biz.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    /**
+     * 解决跨域问题
+     * 方法一：
+     * 使用cors协议解决，此处使用这种方法
+     * 方法二：
+     * 设置 Filter 响应第一个 option 请求
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        /*是否允许请求带有验证信息*/
+        corsConfiguration.setAllowCredentials(true);
+        /*允许访问的客户端域名*/
+        corsConfiguration.addAllowedOrigin("*");
+        /*允许服务端访问的客户端请求头*/
+        corsConfiguration.addAllowedHeader("*");
+        /*允许访问的方法名,GET POST等*/
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+}
